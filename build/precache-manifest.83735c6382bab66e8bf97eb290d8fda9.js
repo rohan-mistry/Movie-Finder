f94a7c03face678c78a40e5fed303406fcb052d9
rohan-mistry <53314296+rohan-mistry@users.noreply.github.com>
2020-05-13 23:56:28 +0530

    added redirect files

---
+++ build/precache-manifest.83735c6382bab66e8bf97eb290d8fda9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1a5a01596cea334ea5b091ce7d8974f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6476b5801f81c90defd9",
+    "url": "/static/js/2.999c2596.chunk.js"
+  },
+  {
+    "revision": "a2676171bfdc2c61fc9d",
+    "url": "/static/js/main.1ea3f679.chunk.js"
+  },
+  {
+    "revision": "a55dd83ac54b7972931b",
+    "url": "/static/js/runtime-main.268f62a4.js"
+  }
+]);
